b9e58f18c67505240719e88412246971edc6c73e
Hyojeon Yoon <se1620236@naver.com>
2021-02-01 23:48:33 +0900

    update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': 'https://placeimg.com/64/64/any',
-          'name': '홍길동',
-          'birthday': '000000',
-          'gender': '남자',
-          'job': '대학생'
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/64/any',
-          'name': '홍길동2',
-          'birthday': '000000',
-          'gender': '남자',
-          'job': '프로그래머'
-        },
-        {
-          'id': 3,
-          'image': 'https://placeimg.com/64/64/any',
-          'name': '홍길동3',
-          'birthday': '000000',
-          'gender': '남자',
-          'job': '디자이너'
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data)
+const { Pool, Client } = require('pg');
+
+const client = new Client({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+client.connect();
+
+app.get('/api/customers', (req, response) => {
+    client.query(
+        'SELECT * FROM CUSTOMER',
+        (err, result) => {
+            response.send(result.rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
